c61a5c732b9d733bd72f83dc2e3ee705e6ad23f7
KhalilMinalang <minalang.khalil@gmail.com>
2022-08-04 14:09:48 +0800

    COMPLETED: Angular 2 Tutorial #6 - Templates & CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 // DECORATOR @
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  templateUrl: 'app.component.html',
+  // can replace with for small projects
+  // template: '<h1>{{name}}</h1>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
