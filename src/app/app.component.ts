0525f07f8883b70617152d26036b3ec2339f08d7
KhalilMinalang <minalang.khalil@gmail.com>
2022-08-04 14:39:37 +0800

    COMPLETED Angular 2 Tutorial #8 - Nesting Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+// IMPORT
+// in ver 14.1.1+ this will throw error
+// import { HomeComponent } from './home/home.component';
+
 // DECORATOR @
 @Component({
   selector: 'app-root',
@@ -7,8 +11,12 @@ import { Component } from '@angular/core';
   templateUrl: 'app.component.html',
   // can replace with for small projects
   // template: '<h1>{{name}}</h1>',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
+  // in ver 14.1.1+ this will throw error
+  // directives: [HomeComponent],
 })
+
 export class AppComponent {
   title = 'app works!, woop woop.';
   name = 'ninja';
