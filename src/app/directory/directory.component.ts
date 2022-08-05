bfb0cd3a9fd32793e6961e37d59df171ae3c0fbe
KhalilMinalang <minalang.khalil@gmail.com>
2022-08-05 18:14:50 +0800

    COMPLETED Angular 2 Tutorial #20 - ngFor

--- src/app/directory/directory.component.ts
+++ src/app/directory/directory.component.ts
@@ -9,14 +9,33 @@ import { ActivatedRoute } from '@angular/router';
 export class DirectoryComponent implements OnInit {
 
   // ninja: string;
-  classes = {
-    'blue': false,
-    'red': false,
-    'underline': true
-  }
+  // classes = {
+  //   'blue': false,
+  //   'red': false,
+  //   'underline': true
+  // }
 
-  test = true;
+  // test = true;
   // test = false;
+
+  ninjas = [
+
+    {
+      name: "Yoshi",
+      belt: "black",
+    },
+
+    {
+      name: "Ryu",
+      belt: "red",
+    },
+
+    {
+      name: "Crystal",
+      belt: "purple",
+    },
+
+  ];
 
   constructor(private route: ActivatedRoute) {
 
